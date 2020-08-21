f94420482e0755d804740815f6c76d9906c7f591
Sharkkii <jaws.sharkkii@gmail.com>
2020-08-21 22:50:57 +0900

    add Jacobi method (eigen value decomposition)

---
+++ LinearAlgebra/jacobi.py
@@ -0,0 +1,52 @@
+#### jacobi (eigen value decomposition) ####
+
+import numpy as np
+
+
+# for scalar
+def is_zero(x):
+    return (np.abs(x) < 1e-8)
+
+
+# for vector 
+def is_zero_vector(x):
+    return (np.linalg.norm(x) < 1e-8)
+
+
+# rotation matrix
+def rotation_matrix(A, theta, i, j):
+    
+    m, n = A.shape
+    assert(m == n)
+    
+    Q = np.eye(n)
+    Q[i, i] = np.cos(theta)
+    Q[i, j] = - np.sin(theta)
+    Q[j, i] = np.sin(theta)
+    Q[j, j] = np.cos(theta)
+    
+    return Q
+
+
+# Jacobi method
+def jacobi_method(A):
+  
+    m, n = A.shape
+    assert(m == n)
+    P = np.eye(n)
+    
+    while (True):
+
+        B = np.abs(A) * (1 - np.eye(n))
+        i, j = np.unravel_index(np.argmax(B), B.shape)
+        if (is_zero(A[i,j])): break
+            
+        theta = np.arctan(2 * A[i,j] / (A[i,i] - A[j,j])) / 2
+        Q = rotation_matrix(A, theta, i, j)
+        A = np.dot(np.dot(Q.T, A), Q)
+        P = np.dot(P, Q)
+        
+    eigen_vals = np.diagonal(A)
+    eigen_vecs = P
+    
+    return eigen_vals, eigen_vecs
