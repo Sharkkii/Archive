117fb787f4ef883f5120a8e77903c7a67750245c
Sharkkii <jaws.sharkkii@gmail.com>
2020-09-02 16:46:18 +0900

    fix a bug on SVD; make it work for input A with #index < #column

--- LinearAlgebra/singular_value_decomposition.py
+++ LinearAlgebra/singular_value_decomposition.py
@@ -1,8 +1,8 @@
 #### Singular Value Decomposition ####
 
 import numpy as np
-from jacobi import jacobi_method
 from gram_schmidt import gram_schmidt
+from eigen_value_decomposition import eigen_value_decomposition
 
 
 # for scalar
@@ -19,11 +19,12 @@ def is_zero_vector(x):
 def singular_value_decomposition(A):
     
     m, n = A.shape
-    if (m < n):
+    flag = (m < n)
+    if (flag):
         A = A.T
         m, n = n, m
         
-    eigen_vals, eigen_vecs = jacobi_method(np.dot(A.T, A))
+    eigen_vals, eigen_vecs = eigen_value_decomposition(np.dot(A.T, A))
     rank = np.argsort(eigen_vals)[::-1]
     Lambda = eigen_vals[rank]
     V = eigen_vecs[:, rank]
@@ -36,9 +37,8 @@ def singular_value_decomposition(A):
     U = gram_schmidt(U_k)
     Sigma = np.dot(U.T, np.dot(A, V))
     
-    if (m < n):
-        U = V.T
+    if (flag):
+        U, V = V, U
         Sigma = Sigma.T
-        V = U.T
     
     return U, Sigma, V
